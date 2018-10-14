f870983dd5e0875f923e0856f383f04c515fd885
Silberdraht <Lord_silberdraht@gmx.de>
2018-10-14 16:27:25 +0200

    Add Class for LinkedLists

---
+++ Automat.h
@@ -0,0 +1,142 @@
+//
+// Created by Silberdraht on 09.10.2018.
+//
+
+#ifndef SYSPROG_AUTOMATA_H
+#define SYSPROG_AUTOMATA_H
+
+#endif //SYSPROG_AUTOMATA_H
+
+#include "LinkedList.h"
+
+
+class Automat {
+
+private:
+    enum state {
+        init,
+        final,
+        error,
+        intstate,
+        idstate,
+        ifstate,
+        whilestate,
+        signstate,
+        mixedstate
+
+    };
+
+    struct tokenInt {
+
+        int line;
+        int column;
+        int value;
+
+
+    };
+
+    struct tokenIdent {
+
+        int line;
+        int column;
+        char lexem[];
+
+
+    };
+
+    struct tokenError {
+        int line;
+        int column;
+
+    };
+
+
+    struct tokenSign {
+
+        char type[];
+        int line;
+        int column;
+    };
+
+    int currentLine = 1;
+    int currentColumn = 0;
+
+    state activState = init;
+
+
+public:
+    Automat();
+
+
+    void checkSymbol(char c) {
+
+        list.addSymbol(c);
+
+        char symbol = c;
+
+        if (1) {
+
+
+
+        }
+
+
+    }
+
+private:
+    LinkedList list;
+
+
+/**
+    struct tokenPlus {
+
+        int line;
+        int column;
+    };
+
+    struct tokenMinus {
+
+        int line;
+        int column;
+    };
+
+    struct tokenColon {
+
+        int line;
+        int column;
+    };
+
+    struct tokenStar {
+
+        int line;
+        int column;
+
+    };
+
+    struct tokenLessSign {
+
+        int line;
+        int column;
+
+    };
+
+    struct tokenMoreSign {
+
+        int line;
+        int column;
+
+    };
+
+    struct tokenSemic {
+
+        int line;
+        int column;
+
+    };
+*/
+
+
+
+
+
+};
