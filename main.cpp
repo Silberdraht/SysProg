f3a9c197f618c85b088ef1384f60b79e6bff3146
Silberdraht <Lord_silberdraht@gmx.de>
2018-10-18 14:37:41 +0200

    working @ automat

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 int main() {
 
-    //LinkedList list;
 
     Automat automat;
 
@@ -13,35 +12,6 @@ int main() {
     automat.checkSymbol('7');
     automat.checkSymbol('x');
 
-
-
-    /*
-
-    list.addSymbol('x');
-    list.addSymbol('t');
-    list.addSymbol('s');
-    list.addSymbol('e');
-    list.addSymbol('t');
-
-
-
-
-
-
-    std::cout << std::boolalpha;
-    std::cout << list.isEmpty() << std::endl;
-    std::cout << list.popSymbol() << std::endl;
-    std::cout << list.popSymbol() << std::endl;
-    std::cout << list.popSymbol() << std::endl;
-    std::cout << list.popSymbol() << std::endl;
-    std::cout << list.popSymbol() << std::endl;
-    std::cout << list.popSymbol() << std::endl;
-    std::cout << list.isEmpty() << std::endl;
-     */
-
-    //list.~LinkedList();
-
-    //automat.~Automat();
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
